f1d1afe2addb68cba41e7b4bc0ac3330a8398ce4
daniboysc <daniel.valenzuela@softcrum.company>
2016-01-14 07:55:37 -0300

    Carga inicial del proyecto

---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('clientIsitelWebsiteApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function(scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
